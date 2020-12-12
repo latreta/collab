5b8a89e9999045afb82c78448cf02b601b6ca50f
latreta <yveshenr@gmail.com>
2020-12-12 01:44:56 -0300

    Recuperando commits e exibindo dados

--- backend/repositories/views.py
+++ backend/repositories/views.py
@@ -2,8 +2,10 @@ from allauth.socialaccount.models import SocialToken, SocialAccount
 from django.http import HttpResponse
 from django.core import serializers
 from github import Github
+from datetime import datetime, timedelta
 
 from .models import Repository
+from commits.models import Commit
 
 
 def index(request):
@@ -11,10 +13,14 @@ def index(request):
     socialToken = SocialToken.objects.filter(account=account)[:1].values("token")
     userToken = socialToken[0]['token']
     g = Github(userToken)
-
-    specificRepo = g.get_repo("latreta/pomodoroApp")
-    for commit in specificRepo.get_commits():
-        print(commit.commit.message)
+    specificRepo = g.get_repo("latreta/aulas-api")
+    start_date = datetime.now() - timedelta(150)
+    
+    for commit in specificRepo.get_commits(since=start_date):
+        objectCommit = commit.commit
+        print(f'Autor: {objectCommit.author.name}')
+        print(f'Data do commit: {objectCommit.author.date}')
+        print(f'Mensagem: {objectCommit.message[:150]}\n')
 
     repositories = Repository.objects.all()
     return HttpResponse(g.get_user().get_repos(), content_type="text/json-comment-filtered")
