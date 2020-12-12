f3860a45e9b14537db3df339a9234ab90ba99477
latreta <yveshenr@gmail.com>
2020-12-12 01:29:16 -0300

    recuperando a lista de commits do usuario

--- backend/repositories/views.py
+++ backend/repositories/views.py
@@ -11,9 +11,10 @@ def index(request):
     socialToken = SocialToken.objects.filter(account=account)[:1].values("token")
     userToken = socialToken[0]['token']
     g = Github(userToken)
-    # Then play with your Github objects:
-    for repo in g.get_user().get_repos():
-        print(repo.name)
+
+    specificRepo = g.get_repo("latreta/pomodoroApp")
+    for commit in specificRepo.get_commits():
+        print(commit.commit.message)
 
     repositories = Repository.objects.all()
     return HttpResponse(g.get_user().get_repos(), content_type="text/json-comment-filtered")
